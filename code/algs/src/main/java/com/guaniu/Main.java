e2436c303446bb20190a99c135d6cb8bafeaca97
liwei <2750181390@qq.com>
2020-10-03 19:17:08 +0800

    Signed-off-by: liwei <2750181390@qq.com> 初次提交

---
+++ code/algs/src/main/java/com/guaniu/Main.java
@@ -0,0 +1,22 @@
+package com.guaniu;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(findHighestCommonFactor(1, 7));
+    }
+
+    /**
+     * [计算最大公约数（欧几里德算法）]
+     * 题目描述：
+     * 计算两个非负整数p 和q 的最大公约数：若
+     * q 是0，则最大公约数为p。否则，将p 除以
+     * q 得到余数r，p 和q 的最大公约数即为q 和
+     * r 的最大公约数。
+     */
+    private static int findHighestCommonFactor(int p, int q){
+        if (q == 0) { // 被除数为0时，除数即为最大公约数
+            return p;
+        }
+        return findHighestCommonFactor(q, p % q);
+    }
+}
